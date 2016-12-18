aca8d6f02db71104165f0e789a38f9424238d0a9
monkoose <monkoose@protonmail.com>
2016-12-18 13:25:26 +0200

    Add collect.sh

---
+++ collect.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+INSTALL_DIR="$HOME/.local/share/nvim"
+mkdir -pv "nvim"
+mkdir -pv "autoload"
+cp -av "$HOME/.config/nvim/dein.vim" "nvim/"
+cp -av "$HOME/.config/nvim/init.vim" "nvim/"
+cp -av "$HOME/.config/nvim/lightline.vim" "nvim/"
+cp -av "$HOME/.config/nvim/maps.vim" "nvim/"
+cp -av "$HOME/.config/nvim/options.vim" "nvim/"
+cp -av "$HOME/.config/nvim/plugins.vim" "nvim/"
+cp -avr "$INSTALL_DIR/site/autoload/lightline/" "autoload/"
+cp -avr "$INSTALL_DIR/site/ftplugin/" .
+cp -avr "$INSTALL_DIR/site/mysnippets/" .
+echo "Done."
